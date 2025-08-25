383ae5d949c7d30edda6a7d684620dde1a59da5f
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-08-24 23:03:42 -0300

    refactor: reorganize form structure and improve input styling
    - Replace individual InputText components with unified Form component
    - Add focus state styling to remove default outline on inputs
    - Enhance placeholder styling with custom color and typography
    - Improve input padding positioning in CSS structure

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,17 +1,39 @@
-import InputText from '../InputText'
-import './Form.css'
+import InputText from "../InputText";
+import "./Form.css";
 
 const Form = () => {
   return (
-    <section className='form'>
-      <form action=''>
-        <InputText label='Nome' placeholder='Digite seu nome' />
-        <InputText label='Cargo' placeholder='Digite seu cargo' />
-        <InputText label='Imagem' placeholder='Informe o endereço URL da imagem' />
-        <InputText label='Telefone' placeholder='Digite seu telefone' />
+    <section className="form">
+      <form action="">
+        <InputText
+          label="Nome"
+          placeholder="Digite seu nome"
+          name="nome"
+          id="input-nome"
+        />
+        <InputText
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          name="cargo"
+          id="input-cargo"
+        />
+        <InputText
+          label="Imagem"
+          placeholder="Informe o endereço URL da imagem"
+          name="imagem"
+          id="input-imagem"
+          type="url"
+        />
+        <InputText
+          label="Telefone"
+          placeholder="Digite seu telefone"
+          name="telefone"
+          id="input-telefone"
+          type="tel"
+        />
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -1,13 +1,25 @@
-import './InputText.css'
+import "./InputText.css";
 
 const InputText = (props) => {
+  // Gera um ID único baseado no label ou usa um ID fornecido via props
+  const inputId =
+    props.id || props.label?.toLowerCase().replace(/\s+/g, "-") || "input-text";
+  const inputName = props.name || inputId;
+
   console.log(props);
   return (
     <div className="input-text">
-      <label htmlFor="input-text">{props.label}</label>
-      <input type="text" id="input-text" placeholder={props.placeholder} />
+      <label htmlFor={inputId}>{props.label}</label>
+      <input
+        type={props.type || "text"}
+        id={inputId}
+        name={inputName}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default InputText
+export default InputText;
